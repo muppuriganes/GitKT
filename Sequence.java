4160a05719b34878fdb7c531463a8f192d029081
Ganesh <muppuriganesh@gmail.com>
2023-08-22 22:56:57 +0530

    Pascal Triangle

--- Sequence.java
+++ Sequence.java
@@ -1,17 +1,22 @@
+import java.util.Scanner;
 public class Sequence {
     public static void main(String[] args) {
-        int n = 10; // Adjust n for the desired number of terms
-        double sum = 0.0;
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enetr the number : ");
+            int n = sc.nextInt();
+            double sum = 0.0;
 
-        for (int i = 1; i <= n; i++) {
-            if (i % 2 == 1) {
-                sum -= 1.0 / i;
-            } else {
-                sum += 1.0 / i;
+            for (int i = 1; i <= n; i++) {
+                if (i % 2 == 1) {
+                    sum -= 1.0 / i;
+                } else {
+                    sum += 1.0 / i;
+                }
+                System.out.println(sum);
             }
-        }
 
-        System.out.println("Sum of the series: " + sum);
+            System.out.println("Sum of the series: " + sum);
+        }
     }
 }
 
