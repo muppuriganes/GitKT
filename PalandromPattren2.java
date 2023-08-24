b43b1cb25d13ff03e10d70610c810365ffb0f732
Ganesh <muppuriganesh@gmail.com>
2023-08-24 18:10:38 +0530

    PalandromicPattern

---
+++ PalandromPattren2.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+public class PalandromPattren2 {
+    public static void main(String[]args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter the number : ");
+            int n = sc.nextInt();
+
+            for(int i=1; i<=n; i++) {
+                //spaces
+                for(int j=1; j<=n-i; j++) {
+                    System.out.print(" ");
+                }
+
+                for(int j=i; j>=1; j--) {
+                    System.out.print(j);
+                }
+
+                for(int j=2; j<=i; j++) {
+                    System.out.print(j);
+                }
+                System.out.println();
+            }
+
+            for(int i=n; i>=1; i--) {
+                //spaces
+                for(int j=1; j<=n-i; j++) {
+                    System.out.print(" ");
+                }
+
+                for(int j=i; j>=1; j--) {
+                    System.out.print(j);
+                }
+
+                for(int j=2; j<=i; j++) {
+                    System.out.print(j);
+                }
+                System.out.println();
+            }
+        }
+        
+    }
+}
