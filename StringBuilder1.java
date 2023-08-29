0368f2987f5eb60f955f0ff24e05548a71fadd22
Ganesh <muppuriganesh@gmail.com>
2023-08-29 18:35:31 +0530

    ArrayStringLenght

---
+++ StringBuilder1.java
@@ -0,0 +1,13 @@
+
+public class StringBuilder1 {
+    public static void main(String[]args) {
+        StringBuilder sb = new StringBuilder("Ganesh");
+        System.out.println(sb);
+
+        sb.insert(0, 'M');
+        System.out.println(sb)
+
+        
+    }
+
+}
