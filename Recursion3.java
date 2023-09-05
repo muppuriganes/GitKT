626bdb5f8b6d09fbd0c660813cf2d6a9193f8935
Ganesh <muppuriganesh@gmail.com>
2023-09-05 11:57:32 +0530

    Recursion4

---
+++ Recursion3.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Recursion3 {
+    public static void towerOfHanoi(int n, String src, String helper, String dest ) {
+        if (n == 1) {
+            System.out.println("Transfer dist " + n + " from " + src + " to" +dest );
+            return;
+        }
+        towerOfHanoi(n-1, src, dest, helper);
+        System.out.println("Transfer dist " + n + " from " + src + " to" +dest);
+        towerOfHanoi(n-1, helper, src, dest );
+    }
+
+    public static void main(String[]args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter N: ");
+        int n = sc.nextInt();
+        towerOfHanoi(n, "S", "H", "D");
+        
+    }
+}
---
+++ Recursion4.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Recursion4 {
+    public static void printRev(String str, int x) {
+        if (x == 0) {
+            System.out.print(str.charAt(x));
+            return;
+        }
+
+        System.out.print(str.charAt(x));
+        printRev(str, x-1);
+
+    }
+
+    public static void main(String[]args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter N: ");
+        String str = sc.next();
+        printRev(str, str.length()-1);
+    }
+}
